aa349149a680350b253a10af45bcb284c48c5111
Adam Wałach <adam.walach@sap.com>
2019-06-18 09:00:04 +0200

    Apply fixes after rebase
    Fixes:
    * makefile targets,
    * instance deprovision operation
    * entries under additionalPrinterColumns in crds.yaml
    * unit tests after rebase

--- pkg/controller/case_test.go
+++ pkg/controller/case_test.go
@@ -30,6 +30,7 @@ import (
 	scinterface "github.com/kubernetes-sigs/service-catalog/pkg/client/clientset_generated/clientset/typed/servicecatalog/v1beta1"
 	scinformers "github.com/kubernetes-sigs/service-catalog/pkg/client/informers_generated/externalversions"
 	"github.com/kubernetes-sigs/service-catalog/pkg/controller"
+	"github.com/kubernetes-sigs/service-catalog/pkg/webhook/servicecatalog/clusterserviceclass/mutation"
 	"github.com/pmorie/go-open-service-broker-client/v2"
 	osb "github.com/pmorie/go-open-service-broker-client/v2"
 	fakeosb "github.com/pmorie/go-open-service-broker-client/v2/fake"
@@ -631,6 +632,19 @@ func (ct *controllerTest) AssertClusterServiceClassAndPlan(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+// AssertClusterServiceClassAndPlan verifies the both the ClusterServiceClass and ClusterServicePlan are present
+func (ct *controllerTest) SetClusterServiceClassLabels() error {
+	csHandler := mutation.CreateUpdateHandler{}
+
+	csc, err := ct.scInterface.ClusterServiceClasses().Get(testClassExternalID, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	csHandler.SyncLabels(csc)
+	_, err = ct.scInterface.ClusterServiceClasses().UpdateStatus(csc)
+	return err
 }
 
 // SetCatalogReactionError sets the catalog call to always return an error
