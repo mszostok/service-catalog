020cc0c0cff3a74db1186edbb1b78124aebebe4d
Mateusz Szostok <szostok.mateusz@gmail.com>
2019-09-06 00:52:27 +0200

    Update handler.go

--- pkg/webhook/servicecatalog/clusterservicebroker/mutation/handler.go
+++ pkg/webhook/servicecatalog/clusterservicebroker/mutation/handler.go
@@ -22,8 +22,6 @@ import (
 	"net/http"
 
 	sc "github.com/kubernetes-sigs/service-catalog/pkg/apis/servicecatalog/v1beta1"
-	"github.com/kubernetes-sigs/service-catalog/pkg/webhookutil"
-
 	admissionTypes "k8s.io/api/admission/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
@@ -37,67 +35,29 @@ type CreateUpdateHandler struct {
 var _ admission.Handler = &CreateUpdateHandler{}
 var _ admission.DecoderInjector = &CreateUpdateHandler{}
 
-// Handle handles admission requests.
 func (h *CreateUpdateHandler) Handle(ctx context.Context, req admission.Request) admission.Response {
-	traced := webhookutil.NewTracedLogger(req.UID)
-	traced.Infof("Start handling mutation operation: %s for %s: %q", req.Operation, req.Kind.Kind, req.Name)
-
 	cb := &sc.ClusterServiceBroker{}
-	if err := webhookutil.MatchKinds(cb, req.Kind); err != nil {
-		traced.Errorf("Error matching kinds: %v", err)
-		return admission.Errored(http.StatusBadRequest, err)
-	}
-
 	if err := h.decoder.Decode(req, cb); err != nil {
-		traced.Errorf("Could not decode request object: %v", err)
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
-	mutated := cb.DeepCopy()
 	switch req.Operation {
 	case admissionTypes.Create:
-		h.mutateOnCreate(ctx, mutated)
+		h.mutateOnCreate(ctx, cb)
 	case admissionTypes.Update:
 		oldObj := &sc.ClusterServiceBroker{}
 		if err := h.decoder.DecodeRaw(req.OldObject, oldObj); err != nil {
-			traced.Errorf("Could not decode request old object: %v", err)
 			return admission.Errored(http.StatusBadRequest, err)
 		}
-		h.mutateOnUpdate(ctx, oldObj, mutated)
+		h.mutateOnUpdate(ctx, oldObj, cb)
 	default:
-		traced.Infof("ClusterServiceBroker mutation wehbook does not support action %q", req.Operation)
 		return admission.Allowed("action not taken")
 	}
 
-	rawMutated, err := json.Marshal(mutated)
+	rawMutated, err := json.Marshal(cb)
 	if err != nil {
-		traced.Errorf("Error marshaling mutated object: %v", err)
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
 
-	traced.Infof("Completed successfully mutation operation: %s for %s: %q", req.Operation, req.Kind.Kind, req.Name)
 	return admission.PatchResponseFromRaw(req.Object.Raw, rawMutated)
-}
-
-// InjectDecoder injects the decoder into the CreateUpdateHandler
-func (h *CreateUpdateHandler) InjectDecoder(d *admission.Decoder) error {
-	h.decoder = d
-	return nil
-}
-
-func (h *CreateUpdateHandler) mutateOnCreate(ctx context.Context, sb *sc.ClusterServiceBroker) {
-	// TODO(mszostok): logic with finalizers was moved from aggregated api-server
-	// question, should we reset whole finalizers entry or only append our own?
-	sb.Finalizers = []string{sc.FinalizerServiceCatalog}
-
-	if sb.Spec.RelistBehavior == "" {
-		sb.Spec.RelistBehavior = sc.ServiceBrokerRelistBehaviorDuration
-	}
-}
-
-func (h *CreateUpdateHandler) mutateOnUpdate(ctx context.Context, oldClusterServiceBroker, newClusterServiceBroker *sc.ClusterServiceBroker) {
-	// Ignore the RelistRequests field when it is the default value
-	if newClusterServiceBroker.Spec.RelistRequests == 0 {
-		newClusterServiceBroker.Spec.RelistRequests = oldClusterServiceBroker.Spec.RelistRequests
-	}
 }
